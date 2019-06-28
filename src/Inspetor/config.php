79d48a58d6fced48d79495810a5a0af32dce1900
Lourenço Biselli <lourenco@useinspetor.com>
2019-06-28 11:36:59 -0300

    Changed the config file to match the new schemas

--- src/Inspetor/config.php
+++ src/Inspetor/config.php
@@ -10,12 +10,12 @@ return [
         'appId'                       => null,
         'encode64'                    => true,
         'debugMode'                   => false,
-        'inspetorOrderSchema'         => 'iglu:com.inspetor/inspetor_order/jsonschema/1-0-0',
         'inspetorAuthSchema'          => 'iglu:com.inspetor/inspetor_auth/jsonschema/1-0-0',
         'inspetorPassRecoverySchema'  => 'iglu:com.inspetor/inspetor_pass_recovery/jsonschema/1-0-0',
-        'inspetorTransactionSchema'   => 'iglu:com.inspetor/inspetor_transaction/jsonschema/1-0-0',
+        'inspetorSaleSchema'          => 'iglu:com.inspetor/inspetor_sale/jsonschema/1-0-0',
         'inspetorTransferSchema'      => 'iglu:com.inspetor/inspetor_transfer/jsonschema/1-0-0',
-        'inspetorUserSchema'          => 'iglu:com.inspetor/inspetor_account/jsonschema/1-1-0',
+        'inspetorAccountSchema'       => 'iglu:com.inspetor/inspetor_account/jsonschema/1-1-0',
+        'inspetorEventSchema'         => 'iglu:com.inspetor/inspetor_event/jsonschema/1-0-0',
         'inspetorContext'             => 'iglu:com.inspetor/inspetor_context/jsonschema/1-0-0'
     ]
 ];
