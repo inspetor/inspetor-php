f36900183f91a776ad2ba5ecd3faa14e246c2a30
Lourenço Biselli <lourenco.biselli@gmail.com>
2019-09-03 15:45:02 -0300

    Minor bug fix

--- src/Inspetor/config.php
+++ src/Inspetor/config.php
@@ -16,7 +16,7 @@ return [
         'inspetorPassRecoverySchema'  => 'iglu:com.inspetor/inspetor_pass_recovery_backend/jsonschema/1-0-0',
         'inspetorSaleSchema'          => 'iglu:com.inspetor/inspetor_sale_backend/jsonschema/1-0-2',
         'inspetorTransferSchema'      => 'iglu:com.inspetor/inspetor_transfer_backend/jsonschema/1-0-0',
-        'inspetorAccountSchema'       => 'iglu:com.inspetor/inspetor_account_backend/jsonschema/1-0-0',
+        'inspetorAccountSchema'       => 'iglu:com.inspetor/inspetor_account_backend/jsonschema/1-0-1',
         'inspetorEventSchema'         => 'iglu:com.inspetor/inspetor_event_backend/jsonschema/1-0-1',
         'inspetorContext'             => 'iglu:com.inspetor/inspetor_context/jsonschema/1-0-0',
         'ingresseSerializationError'  => 'iglu:com.inspetor/inspetor_serialization_error/jsonschema/1-0-0',
